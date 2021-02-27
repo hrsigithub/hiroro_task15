6bbcc0545991814b53d20dff8b8df5e023e90659
ひろろ <hrsi.ad@gmail.com>
2021-02-27 11:10:49 +0900

    Initial Commit

---
+++ task15/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  task15
+//
+//  Created by Hiroshi.Nakai on 2021/02/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ task15/task15App.swift
@@ -0,0 +1,17 @@
+//
+//  task15App.swift
+//  task15
+//
+//  Created by Hiroshi.Nakai on 2021/02/27.
+//
+
+import SwiftUI
+
+@main
+struct task15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
